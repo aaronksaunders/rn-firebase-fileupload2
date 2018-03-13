dfeae7dc5de3321997db460e3f52f5a16ead3e88
aaron k saunders <aaron@clearlyinnovative.com>
2018-03-13 16:18:23 -0500

    adding tab icons to project

--- android/app/src/main/java/com/in3app/MainApplication.java
+++ android/app/src/main/java/com/in3app/MainApplication.java
@@ -3,6 +3,8 @@ package com.in3app;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.oblador.vectoricons.VectorIconsPackage;
+import com.imagepicker.ImagePickerPackage;
 import com.imagepicker.ImagePickerPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
@@ -24,6 +26,8 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new VectorIconsPackage(),
+            new ImagePickerPackage(),
             new ImagePickerPackage()
       );
     }
--- components/tabs/TabNavigation.js
+++ components/tabs/TabNavigation.js
@@ -1,10 +1,44 @@
 import React from "react";
-import { TabNavigator, StackNavigator } from "react-navigation";
+import {
+  TabNavigator,
+  StackNavigator
+} from "react-navigation";
+import Icon from "react-native-vector-icons/Ionicons";
 
 import HomeTab from "./HomeTab";
 import SettingsTab from "./SettingsTab";
 
 export default TabNavigator({
-  Home: { screen: HomeTab },
-  Settings: { screen: SettingsTab }
-});
+      Home: {
+        screen: HomeTab,
+        navigationOptions: () => ({
+            tabBarIcon: ({
+              tintColor
+            }) => {
+              return <Icon name = "ios-home"
+              color = {
+                tintColor
+              }
+              size = {
+                25
+              }
+              />}
+            })
+        },
+        Settings: {
+          screen: SettingsTab,
+          navigationOptions: () => ({
+              tabBarIcon: ({
+                tintColor
+              }) => {
+                return <Icon name = "ios-information-circle"
+                color = {
+                  tintColor
+                }
+                size = {
+                  25
+                }
+                />}
+              })
+          }
+        });
